d588be30a1dfb3c78a23f3eed9fcef6b5c6fb38f
hamtoddsean <36703939+hamtoddsean@users.noreply.github.com>
2022-11-07 03:31:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import time
 
-from http.server import HTTPServer
-import os
-
-# Get port number from the PORT environment varaible or 3000 if not specified
-port = os.getenv('PORT', 3000)
-
-server = HTTPServer(('0.0.0.0', port), MyServer)
-server.serve_forever()
-
 
 k=1
 while k==1:
